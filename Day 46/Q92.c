68461e6bb5c8ff1d6bfebb293b78227ae6586542
SarthakNawani <isarthaknawani@gmail.com>
2026-09-25 16:30:46 +0530

    Add files via upload

---
+++ Day 46/Q92.c
@@ -0,0 +1,22 @@
+// Find the first repeating lowercase alphabet in a string.
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, j;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        for (j = 0; j < i; j++) {
+            if (str[i] == str[j]) {
+                printf("%c", str[i]);
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
